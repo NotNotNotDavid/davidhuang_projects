b3921416bc0ddf081f64819339f05285beb29f27
NotNotNotDavid <94019195+NotNotNotDavid@users.noreply.github.com>
2021-11-09 15:06:45 -0800

    changed main
    made progress today :))

--- Expressions Assignment/src/Main.java
+++ Expressions Assignment/src/Main.java
@@ -13,19 +13,41 @@ public class Main {
         13
         13
         */
+        //11
+        //20
+        //19.2
+        //-3.0
+        //-114.49096475465882
         //Expression 1
         System.out.println((9 - 4)+ 10/2 - (4 - 8)/3);
+        System.out.println((5)+ 10/2 - (4 - 8)/3);
+        System.out.println(5 + 5 - (4 - 8)/3);
+        System.out.println(5 + 5 - (-4)/3);
+        System.out.println(5 + 5 - (-4)/3);
+        System.out.println(5 + 5 - (-1);
+        System.out.println(10 - (-1));
+        System.out.println(10 + 1);
+        System.out.println(11);
 
-        //Expression 2
-        System.out.println(23%4 + (18 - 3 / 2));
 
-        //Expression 3
-        System.out.println(((8 - 5) + Math.pow(3,4)/5));
+//        //Expression 2
+//        System.out.println(23%4 + (18 - 3 / 2));
+//        System.out.println(3 + (18 - 1));
+//        System.out.println(3 + 17);
+//        System.out.println(20);
+//        //Expression 3
+//        System.out.println(((8 - 5) + Math.pow(3,4)/5));
+//        System.out.println(3 + 16.2);
+//        System.out.println(19.2);
+//
+//        //Expression 4
+//        System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+//        System.out.println(1.0 - 8 + 8.0 / 8 + 9.0 / 3);
+//        System.out.println(-7.0 + 1.0 + 3.0);
+//        System.out.println(-3.0);
 
-        //Expression 4
-        System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
-
-        //Expression 5
-        System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+//
+//        //Expression 5
+//        System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
     }
 }
