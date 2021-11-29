b8b1b3b354f24453de6199344c605d9c2ad182bb
NotNotNotDavid <94019195+NotNotNotDavid@users.noreply.github.com>
2021-11-29 14:57:35 -0800

    first commit
    very not done

---
+++ StringManipulation/src/Main.java
@@ -0,0 +1,68 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        String string1 = "azcbobobegghakl";
+        String string2 = "bobxdfcgvhbjkn";
+
+//        System.out.println(manyVowels(string1));
+        System.out.println(manyBob(string2));
+
+
+    }
+
+    /*
+    Problem 1
+
+    Assume s is a string of lower case characters.
+
+    Write a program that counts up the number of vowels contained in the string s.
+    Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, if s = 'azcbobobegghakl',
+    your program should print:
+
+    Number of vowels: 5
+     */
+
+    public static int manyVowels(String str){
+        int count = 0;
+        int stringLength = str.length();
+        for (int i = 0; i < stringLength; i++) {
+            if (str.substring(i, i + 1).equals("a")){
+                count++;
+            }
+            else if (str.substring(i, i + 1).equals("e")){
+                count++;
+            }
+            else if (str.substring(i, i + 1).equals("i")){
+                count++;
+            }
+            else if (str.substring(i, i + 1).equals("o")){
+                count++;
+            }
+            else if (str.substring(i, i + 1).equals("u")){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public static int manyBob(String str){
+        int count = 0;
+        int stringLength = str.length();
+        for (int i = 0; i < stringLength; i++) {
+            if (i + 2 > stringLength){
+                return count;
+            }
+            if (str.substring(i, i + 1).equals("bob")){
+                count++;
+            }
+            else{
+                return count;
+            }
+        }
+        return count;
+    }
+
+
+}
